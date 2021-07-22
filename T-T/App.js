0e2b4438e1eea350c3da65d20f69f254a07e53d9
arhardy <arhardy@ucsd.edu>
2021-07-22 16:46:22 -0700

    landing page and stack navigation, work in progress yall dont be judging

--- T-T/App.js
+++ T-T/App.js
@@ -1,75 +1,64 @@
 import * as React from "react";
-import { Text, View } from "react-native";
+import { Button, View, Text } from "react-native";
+import { StyleSheet } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import ProfileScreen from "./screens/ProfileScreen";
-import GoalsScreen from "./screens/GoalsScreen";
-import MyWorkoutsScreen from "./screens/MyWorkoutsScreen";
-import SearchScreen from "./screens/SearchScreen";
-import HomeScreen from "./screens/HomeScreen";
-import Ionicons from "react-native-vector-icons/Ionicons";
-import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
+import { createStackNavigator } from "@react-navigation/stack";
+import LoginScreen from "./LoginScreen";
+import Register from "./Register";
+import AppHome from "./AppHome";
 
-const Tab = createBottomTabNavigator();
-export default function App() {
+function LandingPage({ navigation }) {
+  return (
+    <View style={styles.container}>
+      <Text>Landing Page</Text>
+      <Text> </Text>
+      <Button title="Sign Up" onPress={() => navigation.navigate("Register")} />
+      <Text> </Text>
+      <Button
+        title="Login"
+        onPress={() => navigation.navigate("LoginScreen")}
+      />
+      <Text> </Text>
+      <Button
+        title="AppHome(with bottom nav)"
+        onPress={() => navigation.navigate("AppHome")}
+      />
+    </View>
+  );
+}
+
+const Stack = createStackNavigator();
+
+function App() {
   return (
     <NavigationContainer>
-      <Tab.Navigator>
-        <Tab.Screen
-          name="Home"
-          component={HomeScreen}
-          options={{
-            tabBarLabel: "Home",
-            tabBarIcon: ({ color }) => (
-              <MaterialCommunityIcons name="home" color={color} size={26} />
-            ),
-          }}
-        />
-        <Tab.Screen
-          name="My Workouts"
-          component={MyWorkoutsScreen}
-          options={{
-            tabBarLabel: "My Workouts",
-            tabBarIcon: ({ color }) => (
-              <MaterialCommunityIcons
-                name="weight-lifter"
-                color={color}
-                size={26}
-              />
-            ),
-          }}
-        />
-        <Tab.Screen
-          name="Search"
-          component={SearchScreen}
-          options={{
-            tabBarLabel: "Search",
-            tabBarIcon: ({ color }) => (
-              <MaterialCommunityIcons name="magnify" color={color} size={26} />
-            ),
-          }}
-        />
-        <Tab.Screen
-          name="Goals"
-          component={GoalsScreen}
-          options={{
-            tabBarLabel: "Goals",
-            tabBarIcon: ({ color }) => (
-              <MaterialCommunityIcons name="calendar" color={color} size={26} />
-            ),
-          }}
-        />
-        <Tab.Screen
-          name="Profile"
-          component={ProfileScreen}
-          options={{
-            tabBarLabel: "Profile",
-            tabBarIcon: ({ color }) => (
-              <MaterialCommunityIcons name="account" color={color} size={26} />
-            ),
-          }}
-        />
-      </Tab.Navigator>
+      <Stack.Navigator initialRouteName="LandingPage">
+        <Stack.Screen name="LandingPage" component={LandingPage} />
+        <Stack.Screen name="LoginScreen" component={LoginScreen} />
+        <Stack.Screen name="Register" component={Register} />
+        <Stack.Screen name="AppHome" component={AppHome} />
+      </Stack.Navigator>
     </NavigationContainer>
   );
 }
+
+export default App;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "turquoise",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: "bold",
+    color: "black",
+  },
+  separator: {
+    marginVertical: 30,
+    height: 1,
+    width: "80%",
+  },
+});
