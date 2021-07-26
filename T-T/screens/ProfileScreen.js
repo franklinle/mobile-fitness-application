bc5031fbc492deb7605ca8480a132777f8dad561
cade sachel <ericchen357@gmail.com>
2021-07-25 20:25:26 -0700

    the profile page

--- T-T/screens/ProfileScreen.js
+++ T-T/screens/ProfileScreen.js
@@ -1,33 +1,72 @@
 import * as React from "react";
 import { StyleSheet } from "react-native";
-import { Text, View } from "react-native";
+import { Text, View, Image } from "react-native";
+
+{/* <Image source = {require('./LandingScreen.png')} style={styles.icon} />
+<Image source = {require('./PlaceholderImg.png')} style={styles.graph} /> */}
+
+
 export default function ProfileScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Profile</Text>
-      <View
-        style={styles.separator}
-        lightColor="#eee"
-        darkColor="rgba(255,255,255,0.1)"
-      />
+      <View style={styles.header}> 
+        <View style= {styles.icon}>
+          <Image source = {require('./LandingScreen.png')} style={{height:"100%", width:"100%", resizeMode:"cover"}} />
+        </View>
+        <View style= {styles.graph}>
+          <Image source = {require('./PlaceholderImg.png')} style={{height:"100%", width:"100%", resizeMode:"cover"}} />
+        </View>
+      </View>
+      <View style={styles.body}>
+        <View style ={styles.activityName}>
+          <Text style={{textAlign: "center", textAlignVertical: "bottom", top:"40%", color:"white"}}>ACTIVITY (what is this?)</Text>
+        </View>
+        <View style={styles.calendar}>
+          <Text style={{textAlign: "center", textAlignVertical: "bottom", top:"40%"}}>Insert some sort of image thingy magjig that connects to calendar</Text>
+        </View>
+      </View>
     </View>
   );
-}
+};
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "turquoise",
-    alignItems: "center",
-    justifyContent: "center",
+    flexDirection: "column",
   },
-  title: {
-    fontSize: 20,
-    fontWeight: "bold",
-    color: "black",
+  header: {
+    flex: 1,
+    flexDirection: "row",
+    backgroundColor: "darkorange",
+    justifyContent: "space-around"
   },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: "80%",
+  icon: {
+    marginTop:40,
+    backgroundColor: "pink",
+    flexBasis:"30%",
+    overflow: "hidden",
+    marginBottom:20,
   },
+  graph: {
+    marginTop:40,
+    backgroundColor: "red",
+    flexBasis: "60%",
+    overflow: "hidden",
+    marginBottom:20,
+  },
+  body: {
+    flex: 3,
+    backgroundColor: "green",
+    flexDirection: "column",
+    justifyContent: "space-around",
+    padding:20,
+  },
+  calendar: {
+    height:"85%",
+    backgroundColor: "orange"
+  },
+  activityName: {
+    backgroundColor: "gray",
+    height:"10%",
+  }
 });
